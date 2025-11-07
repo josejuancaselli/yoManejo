fbbc633393ee2ef12b816664f1b753ce2de0ea74
josecaselli <josejuancaselli@gmail.com>
2025-11-07 00:41:29 -0300

    solo apellido

--- src/components/checkTurnos/seccionZona/BotonesHora.jsx
+++ src/components/checkTurnos/seccionZona/BotonesHora.jsx
@@ -48,7 +48,7 @@ const BotonesHora = ({ ventanaRef, zona, activeHora, setActiveHora, mañanaTarde
                                     </button>
                                     {activeHora === hora && ventanaDireccion && (
                                         <div className="ventana-reservado-modal" onClick={(e) => e.stopPropagation()}>
-                                            <p>{ventanaDireccion.nombre}</p>
+                                            <p>{ventanaDireccion.nombre.split(" ")[ventanaDireccion.nombre.split(" ").length - 1]}</p>
                                             <p>{ventanaDireccion.direccion["calle"]} {ventanaDireccion.direccion["entrecalles"]}</p>
                                         </div>
                                     )}
@@ -101,7 +101,7 @@ const BotonesHora = ({ ventanaRef, zona, activeHora, setActiveHora, mañanaTarde
 
                                     {activeHora === hora && ventanaDireccion && (
                                         <div className="ventana-reservado-modal" onClick={(e) => e.stopPropagation()}>
-                                            <p>{ventanaDireccion.nombre}</p>
+                                            <p>{ventanaDireccion.nombre.split(" ")[ventanaDireccion.nombre.split(" ").length - 1]}</p>
                                             <p>{ventanaDireccion.direccion["calle"]} {ventanaDireccion.direccion["altura"]}</p>
 
                                         </div>
